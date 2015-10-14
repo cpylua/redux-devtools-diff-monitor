cecece98f1c8e3e77749b9dc3e65b1742075022e
Li Chen <zombiefml@gmail.com>
2015-10-14 18:50:06 +0800

    Fix style

--- src/style.js
+++ src/style.js
@@ -4,12 +4,14 @@ export default {
     fontSize: '13px',
     color: 'black',
     maxWidth: '400px',
+    height: '100%',
     background: '#eee',
     padding: '10px',
     overflow: 'auto'
   },
 
   controls: {
+    marginBottom: '10px'
   },
 
   hidden: {
